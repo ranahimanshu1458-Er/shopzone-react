d7e7a96828fb608b2166f1b501bbbcb9bc7c99cc
Himanshu Rana <himanshurana@Himanshus-MacBook-Air.local>
2026-09-19 20:19:32 +0530

    Prepare backend for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5001;
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+  app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 });
